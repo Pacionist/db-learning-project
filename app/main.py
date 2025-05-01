b2a3c4e5297141918c6dd4e033ed6a76422e074e
Pacionist <pacionist@pacionist.ru>
2025-05-01 15:25:04 +0400

    added posibility for creating db from web

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
 from app.users.users import users_router
+from app.db.core import init_db
 
 
 app = FastAPI()
@@ -24,3 +26,8 @@ templates = Jinja2Templates(directory="app/src/templates")
 @app.get("/")
 async def main_page(request: Request):
     return templates.TemplateResponse("index.html", context={"request": request})
+
+@app.post("/init_db")
+async def create_or_reset_db(request: Request):
+    await init_db()
+    return JSONResponse({"Status": "Database init succesful"}, status_code=201)
